6ea671d80d2f52dbc7a5fa250bcabee55a7b07f6
Muukii <muukii.app@gmail.com>
2018-09-08 20:23:35 +0900

    Init

---
+++ GlossButtonNode/GlossButtonNode.h
@@ -0,0 +1,19 @@
+//
+//  GlossButtonNode.h
+//  GlossButtonNode
+//
+//  Created by muukii on 8/3/18.
+//  Copyright © 2018 eure. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GlossButtonNode.
+FOUNDATION_EXPORT double GlossButtonNodeVersionNumber;
+
+//! Project version string for GlossButtonNode.
+FOUNDATION_EXPORT const unsigned char GlossButtonNodeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GlossButtonNode/PublicHeader.h>
+
+
